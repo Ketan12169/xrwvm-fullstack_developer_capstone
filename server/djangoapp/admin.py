724c2ea5e4cf8e112674cf694dad6e6f9e091bb4
Ketan12169 <ketansingh890@gmail.com>
2024-10-09 19:29:47 +0530

    Commit

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,24 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarModelInline class
 
 # CarModelAdmin class
+# @admin.register(CarMake)
+# class CarMakeAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'description')
+#     search_fields = ('name',)
+
+# @admin.register(CarModel)
+# class CarModelAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'car_make', 'type', 'year')
+#     list_filter = ('car_make', 'type')
+#     search_fields = ('name',)
 
 # CarMakeAdmin class with CarModelInline
 
